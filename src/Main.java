a3a0da166f2520613694f83930c1647417ef065f
Bernardo Sampaio <119073813+bernsamp@users.noreply.github.com>
2023-03-18 15:37:09 +0000

    Added program that outputs the Perfect Square numbers within a given range

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void perfectSquares(int a, float b){
+
+        int number = (int) Math.ceil(Math.sqrt(a));
+
+        int number2 = number * number;
+
+        number = (number * 2) + 1;
+
+        while ((number2 >= a && number2 <= b)){
+
+            System.out.print(number2 + " ");
+
+            number2 += number;
+
+            number += 2;
+        }
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter starting range:");
+        int startingRange = sc.nextInt();
+
+        System.out.println("Enter ending range:");
+        int endingRange = sc.nextInt();
+
+        perfectSquares(startingRange, endingRange);
+    }
+}
